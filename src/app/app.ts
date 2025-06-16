3f3ae79c4cb8c549f3f0abf0d0543d0ee20749ca
Vishal <kukadiyavishal55@gmail.com>
2025-06-16 22:59:28 +0530

    -> Add images and enhance app layout with new sections and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,10 +11,98 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected title = 'husher';
 
-  itemsList: { name: string, imageUrl: string }[] = [
+  itemsList: { name: string, imageUrl: string, isActive: boolean }[] = [
     {
-      name: "Main Structure",
-      imageUrl: "Media/images/product-item/image1.png"
+      name: "Main Structural Frame",
+      imageUrl: "Media/images/product-item/image1.png",
+      isActive: false
+    },
+    {
+      name: "Bottom Neoprene Sound Barier",
+      imageUrl: "Media/images/product-item/image2.png",
+      isActive: false
+    },
+    {
+      name: "Upper Neoprene Lip",
+      imageUrl: "Media/images/product-item/image3.png",
+      isActive: false
+    },
+    {
+      name: "Interior Sound Damping Lining",
+      imageUrl: "Media/images/product-item/image4.png",
+      isActive: false
+    },
+    {
+      name: "Velcro for Microphone",
+      imageUrl: "Media/images/product-item/image5.png",
+      isActive: false
+    },
+    {
+      name: "Universal Clamp",
+      imageUrl: "Media/images/product-item/image6.png",
+      isActive: false
+    },
+    {
+      name: "Screw",
+      imageUrl: "Media/images/product-item/image7.png",
+      isActive: false
+    },
+    {
+      name: "Wing Nuts",
+      imageUrl: "Media/images/product-item/image8.png",
+      isActive: false
+    },
+    {
+      name: "0.5 in Clamp Spacers",
+      imageUrl: "Media/images/product-item/image9.png",
+      isActive: false
     }
   ];
+
+  setupItemList = [
+    {
+      name: "Side Address",
+      imageUrl: "Media/images/microphone-setup/image1.jpg",
+      isActive: true
+    },
+    {
+      name: "Classic Dynamic",
+      imageUrl: "Media/images/microphone-setup/image2.jpg",
+      isActive: false
+    },
+    {
+      name: "Side of the snare combo",
+      imageUrl: "Media/images/microphone-setup/image3.jpg",
+      isActive: false
+    },
+    {
+      name: "Twin Perspective",
+      imageUrl: "Media/images/microphone-setup/image4.jpg",
+      isActive: false
+    },
+    {
+      name: "Dynamic & Pencil Condenser",
+      imageUrl: "Media/images/microphone-setup/image5.jpg",
+      isActive: false
+    },
+    {
+      name: "Stereo Source Snare",
+      imageUrl: "Media/images/microphone-setup/image6.jpg",
+      isActive: false
+    }
+  ];
+
+  get activeSetupItemImageUrl() {
+    return this.setupItemList.find(f => f.isActive)?.imageUrl
+  }
+
+  setActive(index: number) {
+    this.setupItemList.forEach((f, i) => {
+      if (i == index) {
+        f.isActive = true;
+      } else {
+        f.isActive = false;
+      }
+    })
+  }
 }
